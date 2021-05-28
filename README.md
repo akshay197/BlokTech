6d99b4906b0109b18263e66b54adeb715dec7412
akshay197 <akshay.ak@hotmail.com>
2021-05-28 11:53:22 +0200

    Package en readme update

--- README.md
+++ README.md
@@ -1,12 +1,36 @@
 # Target Finder
 
-Het welbekende spel Call of Duty heeft al een poos een mobiele versie uitgebracht voor op de smartphone. Met meer dan 300 miljoen downloads heeft dit spel zeker wel het hart gewonnen van de spelers wereldwijd.
+Het welbekende spel Call of Duty heeft al een poos een mobiele versie uitgebracht voor op de smartphone. Met meer dan 300 miljoen downloads heeft dit spel zeker wel het hart gewonnen van de spelers wereldwijd. Daarom heb ik een matching applicatie bedacht die je sneller verbindt met spelers over de hele wereld!
+
+# Mijn feature
+Als feature heb ik voor matching gekozen. Als iemand mij liked en ik hem, dan krijgen wij een match! Dit komt dan in een lijstje te staan. Je kan ook swipen door middel van een javascript library genaamd Swipe-listener. 
+
+![Match](images/match.PNG)
 
 # Installatie
-git clone https://github.com/akshay197/BlokTech.git
+Kopieer de repository naar je eigen machine en installeer de bijbehorende package:
+> git clone https://github.com/akshay197/BlokTech.git
+> npm install
 
-npm start
-http://localhost:3000
+Nu kan je de applicatie starten door deze commando uit te voeren:
+> npm start
+> Example app listening at http://localhost:3000
+> Connected to database
+
+# Database
+De database heet myFirstDatabase en de collectie daarin heet users. Een user ziet er zo uit:
+```Java
+_id: ObjectId("007")    ObjectId (automatisch gegenereerd)
+naam: "Akshay Kumar"    String
+leeftijd: "24"          String
+beschrijving: "Test"    String
+locatie: "Amsterdam"    String
+image: "images/image.jpg" String
+likes:                  Array
+    ObjectId("008")     ObjectId
+dislikes:               Array
+    ObjectId("009")     ObjectId
+```
 
 # Documentatie
 Klik [hier](https://github.com/akshay197/BlokTech/wiki) voor de wiki.
--- server.js
+++ server.js
@@ -12,22 +12,21 @@ const nunjucks = require('nunjucks');
 app.use(express.json());
 app.use(express.urlencoded());
 
-// nunjucks
+// nunjucks configuratie
 nunjucks.configure('views', {
     autoescape: true,
     express: app,
 });
 
-// mijn user id
+// mijn user id uit de database zodat er later mee geliked kan worden
 const myId = '60acf87f0f70474365288d7b';
 
+// variabelen die leeg zijn omdat je later voor meerdere scopes nodig hebt
 let myUser = null;
 let database = null;
 let users = null;
 
-// client opzetten
-
-
+// client opzetten/configuratie
 const connectionString = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_CLUSTER}/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(connectionString, {
     useNewUrlParser: true,
@@ -51,15 +50,16 @@ connectToDatabase();
 let displayedUser = [];
 let unknownUsers = [];
 
-// homepage
+// home - route
 app.get('/', (req, res) => {
-
+    // Uit de collectie users, pik ik de user uit die mijn user id heeft  
     users.findOne(
+        // Dit is de query waarin ik mijn user id opvraag dmv mijn id variabel
         {
             _id: ObjectId(myId),
         },
         (err, user) => {
-            if (err) throw err;
+            if (err) throw err; // error laten zien voor als iets fout gaat
             myUser = user;
 
             // zoek naar users die niet dezelfde id hebben als mijn profiel
@@ -68,13 +68,17 @@ app.get('/', (req, res) => {
                 unknownUsers = [];
 
                 // kijk of ik een gebruiker al eerder heb gedisliked of geliked. als dat niet zo is stuur dan de gebruiker naar de unknown user array
+                // Objectid met elkaar vergelijken gaf altijd false dus daarom JSON.stringify
+                // 1e loop door alle gebruikers
                 for (let i = 0; i < allUsers.length; i++) {
                     const otherUserId = JSON.stringify(ObjectId(allUsers[i]._id).valueOf());
+                    // 2e loop voor mijn dislikes door mij
                     for (let j = 0; j < myUser.dislikes.length; j++) {
                         const dislike = JSON.stringify(myUser.dislikes[j]).valueOf();
+                        // 3e loop voor alle likes door mij
                         for (let k = 0; k < myUser.likes.length; k++) {
                             const like = JSON.stringify(myUser.likes[k]).valueOf();
-
+                            // Als een like/dislike niet gelijk is aan een andere user id, dan wordt hij toegevoegd aan de onbekende gebruikers array (unknown users)   
                             if (dislike !== otherUserId && like !== otherUserId) {
                                 unknownUsers.push(allUsers[i]);
                                 console.log(unknownUsers)
@@ -92,9 +96,9 @@ app.get('/', (req, res) => {
         });
 });
 
-// het huidige profiel kunnen liken of disliken
+// het huidige profiel kunnen liken of disliken in de home route
 app.post('/', (req, res) => {
-    if (req.body.preference === 'like') {
+    if (req.body.preference === 'like') { //als de gebruiker een like geeft, dan update hij mijn gelikete profielen door de user id van de getoonde gebruiker er in te stoppen
         users.updateOne(
             { _id: ObjectId(myId) },
             {
@@ -103,26 +107,26 @@ app.post('/', (req, res) => {
                 },
             }
         );
-    } else {
+    } else { // als de gebruiker een dislike geeft, dan update hij mijn gedislikete profielen door de user id van de getoonde gebruiker er in te stoppen // je hebt maar 2 opties: liken of disliken
         users.updateOne(
             { _id: ObjectId(myId) },
             { $push: { dislikes: displayedUser._id } }
         );
     }
 
-    // boolean voor match om te beslissen of het een match is
+    // boolean voor variabel match
     let match = null;
 
-    // check voor een match
+    // loop door de getoonde gebruikers zijn likes
     for (let i = 0; i < displayedUser.likes.length; i++) {
-        if (JSON.stringify(displayedUser.likes[i]).valueOf() === JSON.stringify(ObjectId(myUser._id).valueOf())) {
+        if (JSON.stringify(displayedUser.likes[i]).valueOf() === JSON.stringify(ObjectId(myUser._id).valueOf())) { // als een van de getoonde gebruikers mij liken, dan match=true anders match=false
             match = true;
         } else {
             match = false;
         }
     }
 
-    // stuur gebruiker naar de 'its a match' pagina als er een match is.
+    // stuur gebruiker naar de 'its a match' pagina als er een match is. Een match ontstaat wanneer ik die gebruiker like en zij/hij mij.
     if (req.body.preference === 'like' && match === true) {
         res.redirect('/match')
         console.log('match')
@@ -133,32 +137,32 @@ app.post('/', (req, res) => {
     }
 });
 
+// laat zien dat er een match is ontstaan
 app.get('/match', (req, res) => {
     res.render('match.njk', { displayedUser });
 });
 
-// de matchende profielen worden hier getoond
+// de matchende profielen worden hier getoond in deze route getoond
 app.get('/matches/', (req, res) => {
-
+    // query: zoek eerst voor mijn gebruiker
     users.findOne(
         {
             _id: ObjectId(myId),
         },
         (err, user) => {
             if (err) throw err;
-            myUser = user;
+            myUser = user; // deze user is het zoekresultaat van de bovenstaande query 
 
             // zoek op alle gebruikers die niet de _id hebben van mijn (tijdelijke) gebruiker
-            users.find({ _id: { $ne: ObjectId(myId) } }).toArray((error, allUsers) => {
-                if (error) throw error;
+            users.find({ _id: { $ne: ObjectId(myId) } }).toArray((err, allUsers) => {
+                if (err) throw err;
 
                 // array voor alle matches
                 const userMatches = [];
 
-                // check of een andere gebruiker mij heeft geliked en of ik hem ook heb geliked
-                for (let i = 0; i < allUsers.length; i++) {
-                    for (let j = 0; j < allUsers[i].likes.length; j++) {
-                        for (let k = 0; k < myUser.likes.length; k++) {
+                for (let i = 0; i < allUsers.length; i++) { // loop door alle gebruikers
+                    for (let j = 0; j < allUsers[i].likes.length; j++) { // loop door gelikete gebruikers door andere gebruikers
+                        for (let k = 0; k < myUser.likes.length; k++) { // loop door gelikete gebruikers door mij
                             if (JSON.stringify(allUsers[i].likes[j]).valueOf() === JSON.stringify(ObjectId(myUser._id).valueOf()) && JSON.stringify(myUser.likes[k]).valueOf() === JSON.stringify(ObjectId(allUsers[i]._id).valueOf())) {
                                 // als ik een user heb geliked en hij heeft mij terug geliked, stuur hem dan naar de userMatches array
                                 userMatches.push(allUsers[i])
